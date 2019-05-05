c0bd48fddce2cee8bd4576c7a472b9d992b14760
Takumi Karibe <takumi.k.5610@gmail.com>
2019-05-05 15:22:35 +0900

    define block

--- rust_chain/src/main.rs
+++ rust_chain/src/main.rs
@@ -1,3 +1,19 @@
+struct Block {
+    height: i32,
+    size: i64,
+    header: Header,
+}
+
+struct Header {
+    parent_hash: String,
+    block_hash: String,
+    merkle_root: String,
+    logs_bloom: String,
+    state_root: String,
+    nonce: i32,
+    time_stamp: i64,
+}
+
 fn main() {
     println!("Hello, world!");
 }
