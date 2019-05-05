38ae4f19d9fc4c64390744e7c0080078b655c8d5
shiki <shiki.tak@gmail.com>
2019-05-05 16:16:51 +0900

    init genesis_block

--- rust_chain/src/main.rs
+++ rust_chain/src/main.rs
@@ -1,3 +1,5 @@
+use chrono::Local;
+
 struct Block {
     height: i32,
     size: i64,
@@ -7,13 +9,22 @@ struct Block {
 struct Header {
     parent_hash: String,
     block_hash: String,
-    merkle_root: String,
-    logs_bloom: String,
-    state_root: String,
     nonce: i32,
     time_stamp: i64,
 }
 
 fn main() {
-    println!("Hello, world!");
+    println!("Start Rust Chain!");
+
+   let header = Header { parent_hash: "0x0000000000000000000000000000000000000000".to_string(),
+                          // TODO: PoWで求める
+                          block_hash: "0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3".to_string(),
+                          nonce: 0,
+                          time_stamp: Local::now().timestamp(),
+                        };
+
+    let genesis_block = Block { height: 0,
+                                size: 0,
+                                header: header,
+                              };
 }
