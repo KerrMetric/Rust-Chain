4df6757b470910952bd96433e553d03eb1faf3e2
Takumi Karibe <takumi.k.5610@gmail.com>
2019-05-06 17:40:31 +0900

    add new account

--- rust_chain/core/core/src/account.rs
+++ rust_chain/core/core/src/account.rs
@@ -1,5 +1,18 @@
+extern crate utils;
+extern crate rand;
+use rand::Rng;
+
 pub struct Account {
     pub address: String,
     pub balance: i64,
     pub miner: bool,
+}
+
+impl Account {
+    fn new() -> Self {
+        Self { address: utils::hash::generate(format!("{}", rand::thread_rng().gen_range(1, 10001))),
+            balance: 0,
+            miner: false
+        }
+    }
 }
--- rust_chain/core/core/src/transaction.rs
+++ rust_chain/core/core/src/transaction.rs
@@ -15,7 +15,6 @@ impl Transaction {
     pub fn new() -> Transaction {
         let (to, from) = (rand::thread_rng().gen_range(1, 101), rand::thread_rng().gen_range(1, 101));
         let to_address = utils::hash::generate(format!("{}", to));
-        let to_address = utils::hash::generate(format!("{}", to));
         let from_address = utils::hash::generate(format!("{}", from));
         let value = rand::thread_rng().gen_range(100, 10001);
         let hash = utils::hash::generate(format!("{}{}{}", to_address, from_address, value));
