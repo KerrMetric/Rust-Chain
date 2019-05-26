b3133a9d5d673f92f11672359a7b04adb9d58e51
shiki <shiki.tak@gmail.com>
2019-05-26 16:32:56 +0900

    add pending field in transaction

--- core/src/transaction.rs
+++ core/src/transaction.rs
@@ -1,4 +1,3 @@
-use rand::Rng;
 use utils;
 
 #[derive(Debug, Clone)]
@@ -7,27 +6,16 @@ pub struct Transaction {
     pub from_address: String,
     pub to_address: String,
     pub value: i64,
+    pub pending: bool,
 }
 
 impl Transaction {
-    pub fn dummy_new() -> Transaction {
-        let (to, from) = (rand::thread_rng().gen_range(1, 101), rand::thread_rng().gen_range(1, 101));
-        let to_address = utils::hash::generate(format!("{}", to));
-        let from_address = utils::hash::generate(format!("{}", from));
-        let value = rand::thread_rng().gen_range(100, 10001);
-        let hash = utils::hash::generate(format!("{}{}{}", to_address, from_address, value));
-
-        Transaction { hash: hash,
-                    to_address: to_address,
-                    from_address: from_address,
-                    value: value }
-    }
-
     pub fn new(from_address: String, to_address: String, value: i64) -> Transaction {
         let hash = utils::hash::generate(format!("{}{}{}", to_address, from_address, value));
         Transaction{hash: hash,
                     from_address: from_address,
                     to_address: to_address,
-                    value: value}
+                    value: value,
+                    pending: false}
     }
 }
